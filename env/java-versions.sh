9e5d9cfce18a7f9b129db0697094604819e4384a
Julien Ponge <julien.ponge@gmail.com>
2019-09-19 11:34:12 +0200

    Use Java 11 as a default JDK

--- env/java-versions.sh
+++ env/java-versions.sh
@@ -1,2 +1,3 @@
-[ -s "/Users/jponge/.jabba/jabba.sh" ] && source "/Users/jponge/.jabba/jabba.sh"
-jabba use zulu@1.8
+[ -s "/Users/jponge/.jabba/jabba.sh" ] && \
+source "/Users/jponge/.jabba/jabba.sh" && \
+jabba use zulu@1.11.0-4
