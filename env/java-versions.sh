ac9899bb71b4c2d059a2cd21a6af02cec618354a
Julien Ponge <jponge@redhat.com>
2020-11-04 21:37:53 +0100

    Misc

--- env/java-versions.sh
+++ env/java-versions.sh
@@ -5,3 +5,6 @@ source "/Users/jponge/.jabba/jabba.sh" && \
 jabba use $(jabba ls | grep zulu | grep 1.11 | sort | tail -n 1)
 
 export MAVEN_OPTS=-Xmx1563m
+
+# jbang from jbangdev/tap
+export PATH="/usr/local/opt/jbang/bin:$PATH"
