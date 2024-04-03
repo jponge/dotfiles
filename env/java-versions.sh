1b1476d06cd43db5256c9b5c46503683c5fe819a
Julien Ponge <jponge@redhat.com>
2024-04-03 10:20:08 +0200

    Maven opts

--- env/java-versions.sh
+++ env/java-versions.sh
@@ -1,3 +1,3 @@
 [ -s ~/.sdkman/bin/sdkman-init.sh ] && source ~/.sdkman/bin/sdkman-init.sh
 
-export MAVEN_OPTS="-Xmx4g -XX:TieredStopAtLevel=1"
+export MAVEN_OPTS="-XX:-MaxFDLimit -Xmx4g -XX:TieredStopAtLevel=1"
