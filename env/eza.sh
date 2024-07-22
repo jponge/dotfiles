dac5223b16653f313ac26abc90f8c355c0a53f0b
Julien Ponge <jponge@redhat.com>
2024-07-22 22:15:35 +0200

    exa to eza

---
+++ env/eza.sh
@@ -0,0 +1,11 @@
+# A few aliases for eza, a ls replacement
+alias l="eza --color-scale --color auto --color-scale-mode=fixed --sort Name"
+alias ll="eza --color-scale --color auto --color-scale-mode=fixed --sort Name --long"
+alias la="eza --color-scale --color auto --color-scale-mode=fixed --sort Name --long --all"
+alias lr="eza --color-scale --color auto --color-scale-mode=fixed --sort Name --long --recurse"
+alias lra="eza --color-scale --color auto --color-scale-mode=fixed --sort Name --long --recurse --all"
+alias lt="eza --color-scale --color auto --color-scale-mode=fixed --sort Name --long --tree"
+alias lta="eza --color-scale --color auto --color-scale-mode=fixed --sort Name --long --tree --all"
+
+alias ls="eza --color-scale --color auto --color-scale-mode=fixed --sort Name"
+alias tree="eza --color-scale --color auto --color-scale-mode=fixed --sort Name --tree"
