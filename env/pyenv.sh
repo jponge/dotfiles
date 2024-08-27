fe3f0027ad69d73d02c2b9f6222bd33894d45e27
Julien Ponge <jponge@redhat.com>
2024-08-27 21:08:04 +0200

    Pyenv fixes

--- env/pyenv.sh
+++ env/pyenv.sh
@@ -1,7 +1,4 @@
 export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
-
-# Load pyenv into the shell by adding
-# the following to ~/.zshrc:
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
+
