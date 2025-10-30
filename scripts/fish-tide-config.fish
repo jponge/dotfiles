127867552863f3580d0463a9362eefab8a15be37
Julien Ponge <julien@ponge.org>
2025-10-30 23:12:17 +0100

    Script to configure Tide, because we ignore ~/.config/fish/fish_variables

---
+++ scripts/fish-tide-config.fish
@@ -0,0 +1,2 @@
+#!/usr/bin/env fish
+ tide configure --auto --style=Classic --prompt_colors='True color' --classic_prompt_color=Dark --show_time='24-hour format' --classic_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, character' --prompt_connection=Disconnected --powerline_right_prompt_frame=No --prompt_spacing=Sparse --icons='Many icons' --transient=No
